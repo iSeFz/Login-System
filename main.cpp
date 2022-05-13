5eefe16a2e931b413e3a8b4d22a00c18a89f4f3a
SeifYahia <seifyahia@gmail.com>
2022-05-13 14:48:03 +0200

    Added the main file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+FCAI – Programming 1 – 2022 - Assignment 4
+Group: S13
+Author1 and ID: Seif Yahia - 20210172
+Author2 and ID: Mohamed Amgad - 20210330
+Author3 and ID: Omar Elgammal - 20210271
+Program Name: Login Application
+Last Modification Date: 13 May 2022
+Version: 1.0
+*/
+
+#include "loginapplib.cpp"
+
+int main(){
+    cout << "Welcome to the Login Application program!\n";
+    start();
+}
