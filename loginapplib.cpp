5d47d8fcf0494b1a02b4b242770295491452a220
SeifYahia <seifyahia@gmail.com>
2022-05-15 21:45:22 +0200

    Added registration related functions

--- loginapplib.cpp
+++ loginapplib.cpp
@@ -23,7 +23,7 @@ void start(){
         cin.clear();
         getline(cin, choice);
         if(choice == "1"){
-            ; // some function
+            registration();
         }
         else if(choice == "2"){
             ; // some function
@@ -100,4 +100,91 @@ void copyData(vector <string>& vector){
     	vector.push_back(line);
     }
     file.close();
+}
+
+// Check for the correct format of the user's name
+string& nameCheck(string& name){
+    // Get the name to check for the correct format
+    while(true){
+        cout << "Enter username: ";
+        cin.clear();
+        getline(cin, name);
+        // Check for the regular expression of names
+        regex validName("^[a-zA-Z-_.]+{4,}$");
+        if(!regex_match(name, validName)){
+            cerr << "########## INVALID username format ##########\n";
+            cout << "Username must be: At least 4 characters.\n"
+                    "ONLY upper and lowercase letters.\n"
+                    "Underscore(_), period(.) or hyphen(-)!\n";
+            continue;
+        }
+        else{
+            return name;
+        }
+    }
+}
+
+// Check for the correct format of the user's email
+string& emailCheck(string& email){
+    copyData(DATA);
+    // Get the email to check for the correct format
+    while(true){
+        cout << "Enter email: ";
+        cin.clear();
+        getline(cin, email);
+        // Check for the regular expression of standard email
+        regex validEmail("^[\\w#!%$&+*-/=?.]+@([\\w-]+\\.)+([\\w-]{2,4})*$");
+        if(!regex_match(email, validEmail)){
+            cerr << "########## INVALID email format ##########\n";
+            continue;
+        }
+        else{
+            for(int i = 3; i < DATA.size(); i += 4){
+                if(email == DATA[i]){
+                    cerr << "########## This email is already registered! ##########\n";
+                    cout << "Please use another email.\n";
+                    return emailCheck(email);
+                }
+            }
+            return email;
+        }
+    }
+}
+
+// Check for the correct format of phone number
+string& phoneCheck(string& phone){
+    // Get the phone number to check for the correct format
+    while(true){
+        cout << "Enter phone number: ";
+        cin.clear();
+        getline(cin, phone);
+        // Check for the regular expression of egyptian phone numbers
+        regex validPhoneNum("^01[0125][0-9]{8}$");
+        if(!regex_match(phone, validPhoneNum)){
+            cerr << "########## INVALID phone number ##########\n";
+            cout << "Phone number must be: ONLY 11 digit numbers\n"
+                    "Starting with 010, 011, 012 or 015!\n";
+            continue;
+        }
+        else{
+            return phone;
+        }
+    }
+}
+
+// Register new profile to the system
+void registration(){
+    cout << "---------- Register new user ----------\n";
+    string name, email, phone, pass;
+    // Check the inputs of the user
+    nameCheck(name);
+    emailCheck(email);
+    phoneCheck(phone);
+    UserData user(pass, name, email, phone);
+    cout << "Welcome " << user.userName << "! You've registered successfully!\n";
+    // Store the newly registered user data to the profiles system
+    ofstream dataBase(DB, ios::app);
+    dataBase << user;
+    dataBase.close();
+    cout << "---------------------------------------\n";
 }
