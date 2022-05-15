52b185857493dd26a4b953902d2c9f1c83244684
MohamedAmgad2002 <mohamedamgad233@gmail.com>
2022-05-15 21:23:10 +0200

    Declare some functions and variables create database.txt

--- functions.h
+++ functions.h
@@ -1,9 +1,20 @@
+// functions.h content
+
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
 // Here we will put function prototypes
 
+// Declare data vector to copy the database file into
+vector <string> DATA;
+
+// Declare database text file
+char DB[] = "database.txt";
+
 // Display the main menu for the user to choose from
 void start();
+
+// Copy data from file into a vector
+void copyData(vector<string>&);
 
 #endif
