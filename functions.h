5c82fa8ce8f20adc02ea00eda1d4f87eb6df3439
Omar <omarelgammal2004@gmail.com>
2022-05-15 21:55:07 +0200

    Added password related functions

--- functions.h
+++ functions.h
@@ -9,6 +9,15 @@ vector <string> DATA;
 // Declare database text file
 char DB[] = "database.txt";
 
+// Declare map of characters
+map <char, int> lettersMap{
+    {'A',0},{'B',1},{'C',2},{'D',3},{'E',4},
+    {'F',5},{'G',6},{'H',7},{'I',8},{'J',9},
+    {'K',10},{'L',11},{'M',12},{'N',13},{'O',14},
+    {'P',15},{'Q',16},{'R',17},{'S',18},{'T',19},
+    {'U',20},{'V',21},{'W',22},{'X',23},{'Y',24},{'Z',25}
+};
+
 // Display the main menu for the user to choose from
 void start();
 
@@ -24,7 +33,16 @@ string& emailCheck(string&);
 // Check for the correct format of phone number
 string& phoneCheck(string&);
 
+// Check for the correct format of the user's password
+string& passwordCheck(string&);
+
+// Encrypt password before storing it
+string passEncryption(string&, int);
+
 // Register new profile to the system
 void registration();
+
+// Hide password while the user is entering
+string hidePass(string&);
 
 #endif
