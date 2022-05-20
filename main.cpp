8776976372934adf425d2f6f8e86d2aa9a8947b0
SeifYahia <seifyahia@gmail.com>
2022-05-20 19:15:28 +0200

    Updated version of the main file

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@ Author1 and ID: Seif Yahia - 20210172
 Author2 and ID: Mohamed Amgad - 20210330
 Author3 and ID: Omar Elgammal - 20210271
 Program Name: Login Application
-Last Modification Date: 16 May 2022
-Version: 3.5
+Last Modification Date: 20 May 2022
+Version: 4.0
 */
 
 #include "loginapplib.cpp"
 
 int main(){
-    cout << "---------- Welcome to the Login Application program! ----------\n";
+    cout << "\t\t\tWelcome to the Login Application program!";
     start();
+    return 0;
 }
